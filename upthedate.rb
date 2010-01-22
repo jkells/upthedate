23f10f19f8b8ca9bc525da7695f1dec5f5512642
Jared Kells <jkells@gmail.com>
2010-01-22 22:00:23 +1000

    Revert "/ is not the symbol for minus!"
    This reverts commit 7aabd5724611ccf0723c4d0c0db6e78b3b77fb3e.

--- upthedate.rb
+++ upthedate.rb
@@ -9,12 +9,12 @@ class UTDMonthDay
 end
 
 class Fixnum
-  alias_method :old_division, :/
+  alias_method :old_minus, :/
   def /(n)
     if n.respond_to?(:superclass) && n.superclass == UTDMonth
       UTDMonthDay.new(self, n)
     else
-      old_division n
+      old_minus n
     end
   end
 end
