7aabd5724611ccf0723c4d0c0db6e78b3b77fb3e
Jared Kells <jkells@gmail.com>
2010-01-22 04:14:53 -0700

    / is not the symbol for minus!

--- upthedate.rb
+++ upthedate.rb
@@ -9,12 +9,12 @@ class UTDMonthDay
 end
 
 class Fixnum
-  alias_method :old_minus, :/
+  alias_method :old_division, :/
   def /(n)
     if n.respond_to?(:superclass) && n.superclass == UTDMonth
       UTDMonthDay.new(self, n)
     else
-      old_minus n
+      old_division n
     end
   end
 end
