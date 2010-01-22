50d2495e667d1ade5cd91f6bf2bba0050859e5eb
Jared Kells <jkells@gmail.com>
2010-01-22 22:06:08 +1000

    Woof Woof

--- upthedate.rb
+++ upthedate.rb
@@ -21,18 +21,19 @@ end
 
 class UTDMonth
   def to_i
-    DateTime::ABBR_MONTHNAMES.find_index self.class
+    DateTime::ABBR_MONTHNAMES.find_index self.class.to_s
   end
 end
 
-class Jan < UTDMonth
-end
-
-class Feb < UTDMonth
-end
-
-class May < UTDMonth
-end
-
-class March < UTDMonth
-end
+class Jan < UTDMonth; end
+class Feb < UTDMonth; end
+class Mar < UTDMonth; end
+class Apr < UTDMonth; end
+class May < UTDMonth; end
+class Jun < UTDMonth; end
+class Jul < UTDMonth; end
+class Aug < UTDMonth; end
+class Sep < UTDMonth; end
+class Oct < UTDMonth; end
+class Nov < UTDMonth; end
+class Dec < UTDMonth; end
